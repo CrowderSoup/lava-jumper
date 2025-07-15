a01bc7b51d7966b24739a4ae2c8c7dc0296915c5
Aaron Crowder <aaron.crowder@gmail.com>
2025-07-15 10:02:57 -0600

    Makes app PWA and adds icon as favicon

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'platform-jumper-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/js/game.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(assets);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
